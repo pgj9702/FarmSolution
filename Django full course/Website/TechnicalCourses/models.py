c5176d591f0f58b571cf33b2198686a00dbe469e
skang35 <spencerkang317@gmail.com>
2021-02-22 17:44:13 +0900

    21.02.22 kss

--- Django full course/Website/TechnicalCourses/models.py
+++ Django full course/Website/TechnicalCourses/models.py
@@ -1,16 +1,22 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 class Allcourses(models.Model):
-    coursename=models.CharField(max_length=200)
+    coursename= models.CharField(max_length=200)
     insname=models.CharField(max_length=100)
+    startedfrom=models.DateTimeField('Started from')
     def __str__(self):
         return self.coursename
 
+    def was_published_recently(self):
+        now=timezone.now()
+        return now-datetime.timedelta(days=1)<= self.startedfrom <= now
 
 
 class details(models.Model):
     couse=models.ForeignKey(Allcourses, on_delete=models.CASCADE)
-    sp=models.CharField(max_length=500)
-    il=models.CharField(max_length=500)
+    ct=models.CharField(max_length=500)
+    your_choice=models.BooleanField(default=False)
     def __str__(self):
-        return self.sp
+        return str(self.ct)
